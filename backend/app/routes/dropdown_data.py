e1632d760b2ea359ba661c6c6fc4891039e0ff58
real-br <113850845+real-br@users.noreply.github.com>
2024-11-23 11:21:34 +0100

    fixes

--- backend/app/routes/dropdown_data.py
+++ backend/app/routes/dropdown_data.py
@@ -10,7 +10,7 @@ router = APIRouter()
 async def get_dropdown_data(
     file_name: str,
 ):
-    parent = Path.cwd()
+    parent = Path.cwd() / "app"
     file_path = parent / "dropdown_data/{file_name}.csv".format(file_name=file_name)
     data = []
     async with aiofiles.open(file_path, mode="r") as csvfile:
